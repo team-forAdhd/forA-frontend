5ae062542303a9edb3f50bb7d205b60b95978da4
sebn <sebn1247@gmail.com>
2024-03-23 18:21:30 +0900

    [feat]adjust i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import 'intl-pluralrules'
 import { StatusBar } from 'expo-status-bar'
+import { useTranslation } from 'react-i18next'
 import { StyleSheet, Text, View } from 'react-native'
+import i18n from '../public/locales/i18n'
+import { useEffect } from 'react'
 
 export default function App() {
+    const { t } = useTranslation('login-join')
+
+    useEffect(() => {
+        i18n
+    }, [])
+
     return (
         <View style={styles.container}>
             <Text>Open up App.tsx to start working on your app!</Text>
