9425c00bcf245c2af82cd7c58a536883e94c31e2
sebn <sebn1247@gmail.com>
2024-09-28 21:15:30 +0900

    [fix] remove duplicate  App.tsx

--- App.tsx
+++ App.tsx
@@ -1,28 +1,50 @@
 import 'intl-pluralrules'
 import { useTranslation } from 'react-i18next'
 import i18n from './src/public/locales/i18n'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useFonts } from 'expo-font'
 import { createStackNavigator } from '@react-navigation/stack'
 import AppNavigator from './src/components/navigation'
 import { Provider } from 'mobx-react'
 import profileStore from './src/state/signupState/profileStore'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import React from 'react'
 
 const Stack = createStackNavigator()
 
 export default function App() {
-    useEffect(() => {
-        i18n
-    }, [])
     const [fontsLoaded] = useFonts({
         Pretendard: require('@/public/assets/font/PretendardVariable.ttf'),
         Chab: require('@/public/assets/font/chab.ttf'),
     })
 
+    const [isFirstLaunch, setIsFirstLaunch] = useState<boolean | null>(null)
+
+    useEffect(() => {
+        i18n
+
+        // 첫 진입인지 확인
+        const checkFirstLaunch = async () => {
+            try {
+                const hasLaunched = await AsyncStorage.getItem('hasLaunched')
+                if (hasLaunched === null) {
+                    setIsFirstLaunch(true)
+                    await AsyncStorage.setItem('hasLaunched', 'true')
+                } else {
+                    setIsFirstLaunch(false)
+                }
+            } catch (error) {
+                console.error('Error checking first launch', error)
+            }
+        }
+        checkFirstLaunch()
+    }, [])
+
     return (
         <Provider profileStore={profileStore}>
-            <AppNavigator />
+            <AppNavigator
+                initialRoute={isFirstLaunch ? 'Onboarding' : 'Home'}
+            />
         </Provider>
     )
 }
