fb1ed2aa7be4e7b42406d4e8b64556ea94e65261
yoon349 <j_yo.on_e@sookmyung.ac.kr>
2024-11-07 08:51:00 +0900

    [feat] changing profile API

---
+++ src/api/myPage/putUserNewProfile.ts
@@ -0,0 +1,26 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { apiClient } from '../login/loginApi'
+
+
+export const putUserNewProfile = async () => {
+    try {
+        const token = await AsyncStorage.getItem('accessToken')
+        const response = await apiClient.put(`/user/profile`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+
+        if (response.status === 200) {
+          console.log('회원 프로필 변경 성공')
+    
+          return response.data
+    
+        } else {
+          console.log('응답 실패, 상태 코드:', response.status)
+        }
+    } catch (error) {
+        console.error('Error setting new profile:', error)
+        throw error
+    }
+}
