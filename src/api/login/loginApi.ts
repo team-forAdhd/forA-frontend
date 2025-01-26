169203fb62f6a1e9b1640b4ea723f30ff62479e8
Minhyung Cho <dup33333@hufs.ac.kr>
2025-01-26 18:11:50 +0900

    feat : 엑세스 토큰 전용 인터셉터 추가

--- src/api/login/loginApi.ts
+++ src/api/login/loginApi.ts
@@ -1,8 +1,17 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useAuthStore } from '@/store/authStore'
+import userStore from '@/store/userStore/userStore'
 
 export const apiClient = axios.create({
     baseURL: 'https://foradhd.site/api/v1',
+    timeout: 5000,
+})
+
+apiClient.interceptors.request.use((config: AxiosRequestConfig) => {
+    const accessToken = useAuthStore.getState().accessToken
+    config.headers.Authorization = `Bearer ${accessToken}`
+    return config
 })
 
 // 로그인 함수
@@ -13,14 +22,7 @@ export const Login = async (userId: string, userPw: string) => {
             password: userPw,
         })
         const token = response.data.accessToken
-        AsyncStorage.setItem('accessToken', token) // 토큰 저장
-
-
-        const token2 = await AsyncStorage.getItem('accessToken')
-        console.log(token2)
-        
-        apiClient.defaults.headers.common['Authorization'] = `Bearer ${token}` // 모든 요청 헤더에 토큰 설정
-
+        useAuthStore.getState().login(token)
     } catch (error) {
         console.error('Error logging in:', error)
         throw error
