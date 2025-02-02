8837e93073f933545fef2b1ddfd031a7b1fa5745
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-02 17:41:34 +0900

    feat : inital Page Routing

--- App.tsx
+++ App.tsx
@@ -1,16 +1,9 @@
 import 'intl-pluralrules'
-import { useTranslation } from 'react-i18next'
-import i18n from './src/public/locales/i18n'
-import { useEffect, useState } from 'react'
+import MainNavigation from './src/navigation'
 import { useFonts } from 'expo-font'
-import { createStackNavigator } from '@react-navigation/stack'
-import AppNavigator from './src/components/navigation'
 import { Provider } from 'mobx-react'
 import profileStore from './src/state/signupState/profileStore'
-import AsyncStorage from '@react-native-async-storage/async-storage'
 import React from 'react'
-
-const Stack = createStackNavigator()
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -18,34 +11,9 @@ export default function App() {
         Chab: require('@/public/assets/font/chab.ttf'),
     })
 
-    const [isFirstLaunch, setIsFirstLaunch] = useState<boolean | null>(null)
-
-    useEffect(() => {
-        i18n
-
-        // 첫 진입인지 확인
-        const checkFirstLaunch = async () => {
-            try {
-                const hasLaunched = await AsyncStorage.getItem('hasLaunched')
-                if (hasLaunched === null) {
-                    setIsFirstLaunch(true)
-                    await AsyncStorage.setItem('hasLaunched', 'true')
-                } else {
-                    setIsFirstLaunch(false)
-                }
-            } catch (error) {
-                console.error('Error checking first launch', error)
-            }
-        }
-        checkFirstLaunch()
-    }, [])
-
     return (
         <Provider profileStore={profileStore}>
-            <AppNavigator
-                // initialRoute="Home"
-                initialRoute={isFirstLaunch ? 'Onboarding' : 'Home'}
-            />
+            <MainNavigation />
         </Provider>
     )
 }
