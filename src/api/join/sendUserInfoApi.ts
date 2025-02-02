c4d468bd84cba4bb9aeea687d6b42034bd632627
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-02 17:44:54 +0900

    chore : signup api 설정 변경

--- src/api/join/sendUserInfoApi.ts
+++ src/api/join/sendUserInfoApi.ts
@@ -1,15 +1,18 @@
-import axios, { AxiosError } from 'axios';
-import { API_URL } from '@env';
+import axios, { AxiosError } from 'axios'
+import { API_URL } from '@env'
 
-export const sendUserInfoApi = async (userInfo: any): Promise<void> => {
-  try {
-    await axios.post(`${API_URL}/api/v1/user/sign-up`, userInfo);
-  } catch (error: unknown) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      throw new Error('Error while sending authentication request: ' + axiosError.message);
-    } else {
-      throw new Error('Unknown error occurred while sending authentication request.');
-    }
-  }
+type SignupResponse = {
+    accessToken: string
+    refreshToken: string
+    hasVerifiedEmail: boolean
+}
+export const sendUserInfoApi = async (
+    userInfo: any,
+): Promise<SignupResponse> => {
+    const response = await axios.post(
+        `${API_URL}/api/v1/user/sign-up`,
+        userInfo,
+        { timeout: 3000 },
+    )
+    return response.data
 }
