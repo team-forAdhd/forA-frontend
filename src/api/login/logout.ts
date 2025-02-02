00047866b5ea0a08deeef67976a139756b456cfd
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-02 17:47:43 +0900

    feat : 로그아웃 자체 로직 추가

--- src/api/login/logout.ts
+++ src/api/login/logout.ts
@@ -1,11 +1,11 @@
+import { useAuthStore } from '@/store/authStore'
 import { apiClient } from './loginApi'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const logout = async () => {
     try {
         await apiClient.delete('/auth/logout')
-        await AsyncStorage.removeItem('accessToken')
-        delete apiClient.defaults.headers.common['Authorization']
+        useAuthStore.getState().logout()
     } catch (error) {
         console.error('Error logging out:', error)
         throw error
