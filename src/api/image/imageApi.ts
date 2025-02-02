68d6d9f79323695248bca5426af1751088860875
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-02 17:42:10 +0900

    fix : 이미지 업로드 제대로 안 되던 문제 해결

--- src/api/image/imageApi.ts
+++ src/api/image/imageApi.ts
@@ -1,38 +1,48 @@
-import axios from 'axios';
-import { API_URL } from '@env';
-import userStore from '@/store/userStore/userStore';
+import axios from 'axios'
+import { API_URL } from '@env'
+import userStore from '@/store/userStore/userStore'
+import { Alert, Platform } from 'react-native'
 
 // 이미지 파일을 Blob으로 변환하는 함수
 const getBlob = async (uri: string): Promise<Blob> => {
-  const response = await fetch(uri);
-  const blob = await response.blob();
-  return blob;
-};
+    const response = await fetch(uri)
+    const blob = await response.blob()
+    return blob
+}
 
+export const uploadImageApi = async (
+    imageFile: any,
+    imagePathPrefix: string = 'DEFAULT_IMAGE',
+) => {
+    try {
+        const formData = new FormData()
+        // 파일의 URI로 Blob 변환
+        // Blob을 FormData에 추가 (파일 객체로 처리)
+        formData.append('imageFileList', {
+            uri: imageFile.uri,
+            name: imageFile.fileName,
+            type: imageFile.mimeType,
+        } as any)
 
-export const uploadImageApi = async (imageFile: any, imagePathPrefix: string = 'DEFAULT_IMAGE') => {
-  const formData = new FormData();
+        // 추가 데이터 설정 (JSON 데이터)
+        formData.append('request', JSON.stringify({}))
 
-  try {
-    // 파일의 URI로 Blob 변환
-    const imageBlob = await getBlob(imageFile.uri);
+        // 서버에 POST 요청 보내기
+        const response = await fetch(`${API_URL}/api/v1/files/images`, {
+            method: 'POST',
+            body: formData,
+        })
 
-    // FormData에 이미지 파일 추가 (Blob 형식으로 추가)
-    formData.append('imageFileList', imageBlob, imageFile.name || `photo_${Date.now()}.jpg`);
+        if (response.ok) {
+            const data = await response.json()
+            return data
+        }
 
-    // 추가 정보 설정
-    formData.append('request', JSON.stringify({ imagePathPrefix }));
-
-    const response = await axios.post(`${API_URL}/api/v1/images`, formData, {
-      headers: {
-        'Authorization': `Bearer ${userStore.accessToken}`,
-        'Content-Type': 'multipart/form-data',
-      },
-    });
-
-    return response.data;
-  } catch (error) {
-    console.error('Image upload failed: Error while trying to upload the image:', error);
-    throw error;
-  }
-};
+        const result = await response.text()
+        Alert.alert('이미지 업로드에 실패했습니다. 다시 시도해 주세요')
+        throw new Error(result)
+    } catch (error) {
+        console.error('이미지 업로드 실패:', error)
+        throw error
+    }
+}
