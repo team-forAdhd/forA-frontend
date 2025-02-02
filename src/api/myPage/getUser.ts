0a8b2f9ce7a0f2588cbf3ac69812a6e1764b304f
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-02 17:47:21 +0900

    delete : 인터셉터 설정으로 인한 인스턴스 개별 설정 삭제

--- src/api/myPage/getUser.ts
+++ src/api/myPage/getUser.ts
@@ -1,15 +1,8 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
 import { apiClient } from '../login/loginApi'
 
 const getUser = async () => {
     try {
-        const token = await AsyncStorage.getItem('accessToken')
-        const response = await apiClient.get(`/user`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
-        //console.log(response.data)
+        const response = await apiClient.get(`/user`)
         return response.data
     } catch (error) {
         console.error('Error fetching comments:', error)
--- src/api/review/reviewsApi.ts
+++ src/api/review/reviewsApi.ts
@@ -1,47 +1,39 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
 import { apiClient } from '../login/loginApi'
 
-
 export const getDoctorsApi = async (hospitalId: string) => {
-  try {
-      const token = await AsyncStorage.getItem('accessToken')
-      const response = await apiClient.get(`/hospitals/${hospitalId}/doctors/brief`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      )
-
-      if (response.status === 200) {
-        console.log('병원 리뷰 목록 불러오기 성공')
-        return response.data.doctorList;
-      } else {
-          console.log('응답 실패, 상태 코드:', response.status)
-      }
-  } catch (error) {
-      console.error('Error fetching hospital doctor list:', error)
-      throw error
-  }
-};
-
-
-export const getReviewsApi = async (hospitalId: string, page: number, size: number, sort: string) => {
     try {
-        const token = await AsyncStorage.getItem('accessToken')
-        const response = await apiClient.get(`/hospitals/${hospitalId}/receipt-reviews?page=${page}&size=${size}&sort=${sort}`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          },
+        const response = await apiClient.get(
+            `/hospitals/${hospitalId}/doctors/brief`,
         )
 
         if (response.status === 200) {
-          console.log('병원 리뷰 목록 불러오기 성공')
-          console.log(response.data)
-          
-          return response.data.hospitalReceiptReviewList;
+            console.log('병원 리뷰 목록 불러오기 성공')
+            return response.data.doctorList
+        } else {
+            console.log('응답 실패, 상태 코드:', response.status)
+        }
+    } catch (error) {
+        console.error('Error fetching hospital doctor list:', error)
+        throw error
+    }
+}
+
+export const getReviewsApi = async (
+    hospitalId: string,
+    page: number,
+    size: number,
+    sort: string,
+) => {
+    try {
+        const response = await apiClient.get(
+            `/hospitals/${hospitalId}/receipt-reviews?page=${page}&size=${size}&sort=${sort}`,
+        )
+
+        if (response.status === 200) {
+            console.log('병원 리뷰 목록 불러오기 성공')
+            console.log(response.data)
+
+            return response.data.hospitalReceiptReviewList
         } else {
             console.log('응답 실패, 상태 코드:', response.status)
         }
@@ -49,53 +41,46 @@ export const getReviewsApi = async (hospitalId: string, page: number, size: numb
         console.error('Error fetching hospital review list:', error)
         throw error
     }
-};
-
-
-export async function deleteReviewApi(hospitalReceiptReviewId: string): Promise<void> {
-  try {
-    const token = await AsyncStorage.getItem('accessToken')
-    const response = await apiClient.delete(`/hospitals/receipt-reviews/${hospitalReceiptReviewId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      },
-    )
-
-      if (response.status === 204) {
-        console.log(`영수증 리뷰 삭제 성공 (Review Id: ${hospitalReceiptReviewId})`)
-      } else {
-        console.log('응답 실패, 상태 코드:', response.status)
-      }
-
-  } catch (error) {
-    console.error('Error deleting receipt review:', error)
-    throw error
-  }
-};
-
-
-export async function reviewHelpedApi(hospitalReceiptReviewId: string): Promise<void> {
-  try {
-    const token = await AsyncStorage.getItem('accessToken')
-    const response = await apiClient.post(`/hospitals/receipt-reviews/${hospitalReceiptReviewId}/help?help=true`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      },
-    )
-
-      if (response.status === 200) {
-        console.log(`도움돼요 누르기 성공 (Review Id: ${hospitalReceiptReviewId})`)
-      } else {
-        console.log('응답 실패, 상태 코드:', response.status)
-      }
-
-  } catch (error) {
-    console.error('Error pressing review is helped:', error)
-    throw error
-  }
 }
 
+export async function deleteReviewApi(
+    hospitalReceiptReviewId: string,
+): Promise<void> {
+    try {
+        const response = await apiClient.delete(
+            `/hospitals/receipt-reviews/${hospitalReceiptReviewId}`,
+        )
+
+        if (response.status === 204) {
+            console.log(
+                `영수증 리뷰 삭제 성공 (Review Id: ${hospitalReceiptReviewId})`,
+            )
+        } else {
+            console.log('응답 실패, 상태 코드:', response.status)
+        }
+    } catch (error) {
+        console.error('Error deleting receipt review:', error)
+        throw error
+    }
+}
+
+export async function reviewHelpedApi(
+    hospitalReceiptReviewId: string,
+): Promise<void> {
+    try {
+        const response = await apiClient.post(
+            `/hospitals/receipt-reviews/${hospitalReceiptReviewId}/help?help=true`,
+        )
+
+        if (response.status === 200) {
+            console.log(
+                `도움돼요 누르기 성공 (Review Id: ${hospitalReceiptReviewId})`,
+            )
+        } else {
+            console.log('응답 실패, 상태 코드:', response.status)
+        }
+    } catch (error) {
+        console.error('Error pressing review is helped:', error)
+        throw error
+    }
+}
