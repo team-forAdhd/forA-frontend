1e0ae27964fbd1121657da73663547b8c5b80243
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-02 17:43:42 +0900

    chore : 이메일 중복 확인 파라미터 쿼리파라미터로 변경

--- src/api/join/checkExistingMemberApi.ts
+++ src/api/join/checkExistingMemberApi.ts
@@ -1,22 +1,30 @@
-import axios, { AxiosError, AxiosResponse } from 'axios';
-import { API_URL } from '@env';
+import axios, { AxiosError, AxiosResponse } from 'axios'
+import { API_URL } from '@env'
 
 interface ApiResponse {
-  isExistingMember: boolean;
+    isValidEmail: boolean
 }
 
-export const checkExistingMemberApi = async (email: string): Promise<boolean> => {
-  try {
-    const response: AxiosResponse<ApiResponse> = await axios.post(`${API_URL}/api/v1/user/email-auth`, { email });
-    const isExistingMember = response.data.isExistingMember;
+export const checkExistingMemberApi = async (
+    email: string,
+): Promise<boolean> => {
+    try {
+        const response: AxiosResponse<ApiResponse> = await axios.get(
+            `${API_URL}/api/v1/user/email-check?email=${email}`,
+        )
+        const isExistingMember = response.data.isValidEmail
 
-    return isExistingMember;
-  } catch (error: unknown) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      throw new Error('Error while checking existing member: ' + axiosError.message);
-    } else {
-      throw new Error('Unknown error occurred while checking existing member.');
+        return isExistingMember
+    } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+            const axiosError = error as AxiosError
+            throw new Error(
+                'Error while checking existing member: ' + axiosError.message,
+            )
+        } else {
+            throw new Error(
+                'Unknown error occurred while checking existing member.',
+            )
+        }
     }
-  }
 }
