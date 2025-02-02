24cf6071e07f365b2b29198747205828c94b6e6b
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-02 17:44:26 +0900

    chore : 확인코드 이메일 전송 api 변경

--- src/api/join/sendAuthApi.ts
+++ src/api/join/sendAuthApi.ts
@@ -1,15 +1,20 @@
-import axios, { AxiosError } from 'axios';
-import { API_URL } from '@env';
+import axios, { AxiosError } from 'axios'
+import { API_URL } from '@env'
 
 export const sendAuthApi = async (email: string): Promise<void> => {
-  try {
-    await axios.put(`${API_URL}/api/v1/user/email-auth`);
-  } catch (error: unknown) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      throw new Error('Error while sending authentication request: ' + axiosError.message);
-    } else {
-      throw new Error('Unknown error occurred while sending authentication request.');
+    try {
+        await axios.post(`${API_URL}/api/v1/user/email-auth`, { email })
+    } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+            const axiosError = error as AxiosError
+            throw new Error(
+                'Error while sending authentication request: ' +
+                    axiosError.message,
+            )
+        } else {
+            throw new Error(
+                'Unknown error occurred while sending authentication request.',
+            )
+        }
     }
-  }
 }
