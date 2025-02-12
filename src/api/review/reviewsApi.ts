41eaf6e5682f341500c641c5c0d91c95753581af
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-12 20:06:30 +0900

    fix: 리뷰 api 수정

--- src/api/review/reviewsApi.ts
+++ src/api/review/reviewsApi.ts
@@ -1,22 +1,24 @@
-import { apiClient } from '../login/loginApi'
+import { ImagePickerAsset } from 'expo-image-picker';
+import { apiClient } from '../login/loginApi';
+import { uploadImageApi } from '@/api/image/imageApi';
 
 export const getDoctorsApi = async (hospitalId: string) => {
     try {
         const response = await apiClient.get(
             `/hospitals/${hospitalId}/doctors/brief`,
-        )
+        );
 
         if (response.status === 200) {
-            console.log('병원 리뷰 목록 불러오기 성공')
-            return response.data.doctorList
+            console.log('병원 리뷰 목록 불러오기 성공');
+            return response.data.doctorList;
         } else {
-            console.log('응답 실패, 상태 코드:', response.status)
+            console.log('응답 실패, 상태 코드:', response.status);
         }
     } catch (error) {
-        console.error('Error fetching hospital doctor list:', error)
-        throw error
+        console.error('Error fetching hospital doctor list:', error);
+        throw error;
     }
-}
+};
 
 export const getReviewsApi = async (
     hospitalId: string,
@@ -27,21 +29,21 @@ export const getReviewsApi = async (
     try {
         const response = await apiClient.get(
             `/hospitals/${hospitalId}/receipt-reviews?page=${page}&size=${size}&sort=${sort}`,
-        )
+        );
 
         if (response.status === 200) {
-            console.log('병원 리뷰 목록 불러오기 성공')
-            console.log(response.data)
+            console.log('병원 리뷰 목록 불러오기 성공');
+            console.log(response.data);
 
-            return response.data.hospitalReceiptReviewList
+            return response.data.hospitalReceiptReviewList;
         } else {
-            console.log('응답 실패, 상태 코드:', response.status)
+            console.log('응답 실패, 상태 코드:', response.status);
         }
     } catch (error) {
-        console.error('Error fetching hospital review list:', error)
-        throw error
+        console.error('Error fetching hospital review list:', error);
+        throw error;
     }
-}
+};
 
 export async function deleteReviewApi(
     hospitalReceiptReviewId: string,
@@ -49,18 +51,18 @@ export async function deleteReviewApi(
     try {
         const response = await apiClient.delete(
             `/hospitals/receipt-reviews/${hospitalReceiptReviewId}`,
-        )
+        );
 
         if (response.status === 204) {
             console.log(
                 `영수증 리뷰 삭제 성공 (Review Id: ${hospitalReceiptReviewId})`,
-            )
+            );
         } else {
-            console.log('응답 실패, 상태 코드:', response.status)
+            console.log('응답 실패, 상태 코드:', response.status);
         }
     } catch (error) {
-        console.error('Error deleting receipt review:', error)
-        throw error
+        console.error('Error deleting receipt review:', error);
+        throw error;
     }
 }
 
@@ -70,17 +72,47 @@ export async function reviewHelpedApi(
     try {
         const response = await apiClient.post(
             `/hospitals/receipt-reviews/${hospitalReceiptReviewId}/help?help=true`,
-        )
+        );
 
         if (response.status === 200) {
             console.log(
                 `도움돼요 누르기 성공 (Review Id: ${hospitalReceiptReviewId})`,
-            )
+            );
         } else {
-            console.log('응답 실패, 상태 코드:', response.status)
+            console.log('응답 실패, 상태 코드:', response.status);
         }
     } catch (error) {
-        console.error('Error pressing review is helped:', error)
-        throw error
+        console.error('Error pressing review is helped:', error);
+        throw error;
     }
+}
+
+type PostNewReceiptReviewsRequest = {
+    hospitalId: string;
+    content: string;
+    imageList: ImagePickerAsset[];
+    medicalExpense: number;
+    doctorId?: string;
+};
+export async function postNewReceiptReviews({
+    content,
+    imageList,
+    medicalExpense,
+    hospitalId,
+    doctorId,
+}: PostNewReceiptReviewsRequest) {
+    const REQUEST_URL = doctorId
+        ? `/hospitals/${hospitalId}/receipt-reviews`
+        : `/hospitals/${hospitalId}/doctors/${doctorId}/receipt-reviews`;
+    let imagePathList = imageList;
+    if (imageList.length) {
+        imagePathList = await Promise.all(
+            imageList.map((image) => uploadImageApi(image)),
+        );
+    }
+    apiClient.post(REQUEST_URL, {
+        content,
+        imagePath: imagePathList,
+        medicalExpense,
+    });
 }
