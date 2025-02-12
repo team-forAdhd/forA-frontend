d35e7134942165c316ba3daad05e550e4bd9f709
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-12 20:08:18 +0900

    feat : 로딩모달 컴포넌트 추가

---
+++ src/components/common/modals/loadingModal.tsx
@@ -0,0 +1,25 @@
+import { ActivityIndicator, SafeAreaView, StyleSheet } from 'react-native';
+
+export default function LoadingModal({ visible }: { visible: boolean }) {
+    if (visible)
+        return (
+            <SafeAreaView style={[styles.container, styles.container]}>
+                <ActivityIndicator color={'#52A55D'} size={'large'} />
+            </SafeAreaView>
+        );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    horizontal: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        padding: 10,
+    },
+});
