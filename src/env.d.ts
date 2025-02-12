bf592ba436edef06987caacdf7ccdaff89196c8e
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-12 20:02:46 +0900

    chore : env export d.ts 추가

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,8 @@
 declare module '@env' {
-    export const API_URL: string
-    export const SPREADSHEET_ID: string
-    export const GOOGLE_MAPS_API_KEY: string
-    export const GOOGLE_GEOCODING_API_KEY: string
+    export const API_URL: string;
+    export const SPREADSHEET_ID: string;
+    export const GOOGLE_MAPS_API_KEY: string;
+    export const GOOGLE_GEOCODING_API_KEY: string;
+    export const NAVER_CLOUD_PLATFORM_OCR_SECRET: string;
+    export const NAVER_CLOUD_PLATFORM_OCR_URL: string;
 }
