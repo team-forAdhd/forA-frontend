1012f7623b94db2c05d91f9d346837be3d741b3b
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-24 23:01:15 +0900

    feat : hospitalDescriptor 컴포넌트 추가

---
+++ src/domains/HospitalList/components/hospitalDescriptor.tsx
@@ -0,0 +1,31 @@
+import HospitalListItem, {
+    HospitalItemProps,
+} from '@/components/common/hospitalListItem/hospitalListItem';
+import { Hospital } from '@/domains/HospitalList/screens/HospitalMaps';
+import { Location } from '@/hooks/useLocation';
+import { View } from 'react-native';
+
+export default function HospitalDescriptor({
+    hospital,
+    location,
+}: {
+    hospital: Hospital;
+    location: Location;
+}) {
+    return (
+        <View
+            style={{
+                width: '100%',
+                padding: 10,
+                zIndex: 20,
+                position: 'absolute',
+                bottom: 0,
+                justifyContent: 'center',
+                backgroundColor: 'white',
+                borderRadius: 20,
+            }}
+        >
+            <HospitalListItem hospital={hospital} location={location} />
+        </View>
+    );
+}
