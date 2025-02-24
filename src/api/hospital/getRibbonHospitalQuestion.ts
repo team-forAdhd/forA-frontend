b13932063066af2a738776f8c0cd4a44bcada3fa
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-24 22:54:29 +0900

    fix : question 관련 api 오류 수정

--- src/api/hospital/getRibbonHospitalQuestion.ts
+++ src/api/hospital/getRibbonHospitalQuestion.ts
@@ -1,20 +1,46 @@
 ///api/v1/hospitals/evaluation-questions
-import { apiClient } from '../login/loginApi'
+import { apiClient } from '../login/loginApi';
+import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
 
 export const getRibbonHospitalQuestion = async () => {
-    try {
-        const response = await apiClient.get(`/hospitals/evaluation-questions`)
-        if (response.status === 200) {
-            console.log(
-                '응답 성공:',
-                response.data.hospitalEvaluationQuestionList,
-            )
-        } else {
-            console.log('응답 실패, 상태 코드:', response.status)
-        }
-        return response.data.hospitalEvaluationQuestionList
-    } catch (error) {
-        console.error('Error fetching comments:', error)
-        throw error
-    }
-}
+    const response = await apiClient.get(`/hospitals/evaluation-questions`);
+    return response.data.hospitalEvaluationQuestionList;
+};
+
+type PostEvaluationReviewRequest = {
+    hospitalId: string;
+    questions: boolean[];
+};
+export const postEvalutaionReview = ({
+    hospitalId,
+    questions,
+}: PostEvaluationReviewRequest) => {
+    const evaluationAnswerList = questions.map((ans, idx) => {
+        return {
+            hospitalEvaluationQuestionId: idx + 1,
+            answer: ans,
+        };
+    });
+
+    return apiClient.post(`/hospitals/${hospitalId}/evaluation-reviews`, {
+        hospitalEvaluationAnswerList: evaluationAnswerList,
+    });
+};
+
+export const useEvaluationReviewMutation = (
+    options?: UseMutationOptions<
+        AxiosResponse<any>,
+        unknown,
+        PostEvaluationReviewRequest
+    >,
+) => {
+    return useMutation<
+        AxiosResponse<any>,
+        unknown,
+        PostEvaluationReviewRequest
+    >({
+        mutationFn: (req) => postEvalutaionReview(req),
+        ...options,
+    });
+};
