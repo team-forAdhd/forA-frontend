8e0459d98b2b1a2bd7979059429b62974b39fed5
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-24 22:58:26 +0900

    refactor : imagePathMerge 분기문 추가

--- src/utils/imagePathMerge.ts
+++ src/utils/imagePathMerge.ts
@@ -1,6 +1,7 @@
 import { CDN_CLOUDFRONT_URL } from '@env';
 
 export function imagePathMerge(uri: string) {
+    if (!uri) return;
     const baseURL = CDN_CLOUDFRONT_URL;
     if (!uri.startsWith('/')) return baseURL + '/' + uri;
     return baseURL + uri;
