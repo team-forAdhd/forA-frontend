36d9107bd26c7cc637f0105afceec864449a14f7
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-24 23:43:12 +0900

    feat : 카메라 스크린 OCR 적용

--- src/components/review/CameraScreen.tsx
+++ src/components/review/CameraScreen.tsx
@@ -5,14 +5,16 @@ import {
     Text,
     Modal,
     Pressable,
+    TextStyle,
+    StyleProp,
+    StyleSheet,
 } from 'react-native';
 import { CameraView } from 'expo-camera';
 import React, { useState, useEffect } from 'react';
-import { styles, text } from './CameraScreenStyle';
 import {
     receiptValidationCheck,
     ValidationError,
-} from '@/api/review/cameraOcrApi';
+} from './receiptValidation.api';
 import { StackScreenProps } from '@react-navigation/stack';
 import { HospitalStackParams } from '@/navigation/stacks/hospitalStack';
 import LoadingModal from '@/components/common/modals/loadingModal';
@@ -24,7 +26,7 @@ export default function CameraScreen({
     navigation,
     route,
 }: StackScreenProps<HospitalStackParams, 'CameraScreen'>) {
-    const hospitalInfo = route.params.hospitalInfo;
+    const { hospitalInfo, ribbonEvaluation } = route.params;
     const { cameraRef, facing, takePictureHandler, toggleCameraFacing } =
         useCamera();
 
@@ -37,16 +39,11 @@ export default function CameraScreen({
     const takePicture = async () => {
         try {
             setLoading(true);
-            // const result = await takePictureHandler();
-            // if (!result || (result && !result?.base64)) return;
-
-            // const response = await receiptValidationCheck(
-            //     result?.base64 ?? '',
-            //     hospitalInfo.name,
-            // );
+            const result = await takePictureHandler();
+            if (!result || (result && !result?.base64)) return;
 
             const hospitalPrice = await receiptValidationCheck(
-                '',
+                result?.base64 ?? '',
                 hospitalInfo.name,
             );
             setPrice(hospitalPrice);
@@ -75,10 +72,16 @@ export default function CameraScreen({
     /* 의사 선택 화면으로 넘기는 함수 */
     const pressModalButton = () => {
         setIsModalVisible(false);
-        navigation.navigate('ChooseDoctor', {
-            hospitalInfo: hospitalInfo,
-            price: price ?? 0,
-        });
+        if (ribbonEvaluation) {
+            navigation.push('RibbonEvaluation', {
+                hospitalInfo: hospitalInfo,
+            });
+        } else {
+            navigation.push('ChooseDoctor', {
+                hospitalInfo: hospitalInfo,
+                price: price ?? 0,
+            });
+        }
     };
 
     return (
@@ -91,7 +94,7 @@ export default function CameraScreen({
                 >
                     {/* HEADER */}
                     <View style={styles.headerTextContainer}>
-                        <Text style={text.headerText}>간단리뷰</Text>
+                        <Text style={text.headerText}>{hospitalInfo.name}</Text>
                     </View>
                     <TouchableOpacity
                         activeOpacity={1}
@@ -107,11 +110,35 @@ export default function CameraScreen({
                     </TouchableOpacity>
 
                     {/* MESSAGE BOX */}
-                    <View style={styles.messageBox}>
-                        <Text style={text.messageText}>영수증의 글자가</Text>
-                        <Text style={text.messageText}>
-                            잘 보이게 찍어주세요
-                        </Text>
+                    <View style={styles.messageBoxContainer}>
+                        <View style={styles.informMessageBox}>
+                            <Text style={text.messageText}>
+                                영수증의 글자가
+                            </Text>
+                            <Text style={text.messageText}>
+                                잘 보이게 찍어주세요
+                            </Text>
+                        </View>
+                        {ribbonEvaluation && (
+                            <View style={styles.ribbonMessageBox}>
+                                <Image
+                                    source={require('@/public/assets/ribbon.png')}
+                                    style={styles.ribbon}
+                                />
+                                <Text
+                                    style={{
+                                        color: color.ribbon,
+                                        fontWeight: 'bold',
+                                    }}
+                                >
+                                    포에이 리본 달아주기
+                                </Text>
+                                <Image
+                                    source={require('@/public/assets/ribbon.png')}
+                                    style={styles.ribbon}
+                                />
+                            </View>
+                        )}
                     </View>
 
                     <View style={styles.buttonContainer}>
@@ -188,3 +215,166 @@ export default function CameraScreen({
         </>
     );
 }
+
+const color = {
+    primary: '#52A35D',
+    inactive: '#949494',
+    inactiveBottom: '#EFEFF0',
+    ribbon: '#FF5D5D',
+    normal: '#232323',
+    faintBlack: '#555555',
+    backgroundGray: '#EDEDEA',
+    faintGray: '#EEEEEE',
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    headerTextContainer: {
+        position: 'absolute',
+        top: 52,
+        height: 36,
+        justifyContent: 'center',
+        alignSelf: 'center',
+        alignItems: 'center',
+    },
+    headerButtonContainer: {
+        position: 'absolute',
+        top: 52,
+        height: 36,
+        paddingRight: 20,
+        alignSelf: 'flex-end',
+        alignItems: 'flex-end',
+    },
+    camera: {
+        flex: 1,
+    },
+    messageBoxContainer: {
+        position: 'absolute',
+        top: 120,
+        width: '80%',
+        height: 80,
+        justifyContent: 'center',
+        alignSelf: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        gap: 10,
+    },
+    informMessageBox: {
+        width: '100%',
+        padding: 15,
+        borderRadius: 85,
+        opacity: 0.8,
+        backgroundColor: 'white',
+    },
+    ribbonMessageBox: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 30,
+        gap: 10,
+    },
+    buttonContainer: {
+        alignSelf: 'center',
+        position: 'absolute',
+        bottom: 85,
+        width: '95%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    iconHeaderImage: {
+        width: 42,
+        height: 42,
+        objectFit: 'contain',
+    },
+    iconImage: {
+        width: 45,
+        height: 45,
+        objectFit: 'contain',
+    },
+    ribbon: {
+        color: color.ribbon,
+        width: 20,
+        height: 20,
+    },
+    iconShootImage: {
+        width: 80,
+        height: 80,
+        objectFit: 'contain',
+    },
+    modalBackGround: {
+        flex: 1,
+        backgroundColor: 'black',
+        opacity: 0.85,
+    },
+    modalView: {
+        marginTop: 340,
+        margin: 30,
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        borderRadius: 15,
+        padding: 28,
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+});
+
+const baseText = {
+    fontFamily: 'Pretendard',
+    fontStyle: 'normal',
+    fontWeight: '400',
+};
+
+const text = {
+    headerText: {
+        color: 'white',
+        fontSize: 19,
+        fontWeight: 'bold',
+        letterSpacing: -0.7,
+        lineHeight: 22.4,
+        textAlign: 'center',
+    },
+    messageText: {
+        ...baseText,
+        color: color.normal,
+        fontSize: 19,
+        letterSpacing: -0.7,
+        lineHeight: 22.4,
+        textAlign: 'center',
+    },
+    modalText: {
+        fontSize: 19,
+        fontWeight: '600',
+        lineHeight: 22.4,
+        letterSpacing: -0.7,
+    },
+    boxText: {
+        ...baseText,
+        fontSize: 18,
+        lineHeight: 22.4,
+    },
+    buttonText: {
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: '600',
+        lineHeight: 22.4,
+    },
+} as {
+    [key: string]: StyleProp<TextStyle>;
+};
