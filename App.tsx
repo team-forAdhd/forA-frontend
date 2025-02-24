665737acdde9b9ac4980d98943c50f946c8b70bb
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-24 22:51:18 +0900

    feat: QueryClientProvider 설정

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
-import 'intl-pluralrules'
-import MainNavigation from './src/navigation'
-import { useFonts } from 'expo-font'
-import { Provider } from 'mobx-react'
-import profileStore from './src/state/signupState/profileStore'
-import React from 'react'
+import 'intl-pluralrules';
+import MainNavigation from './src/navigation';
+import { useFonts } from 'expo-font';
+import { Provider } from 'mobx-react';
+import profileStore from './src/state/signupState/profileStore';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
 
+const queryClient = new QueryClient();
 export default function App() {
     const [fontsLoaded] = useFonts({
         Pretendard: require('@/public/assets/font/PretendardVariable.ttf'),
         Chab: require('@/public/assets/font/chab.ttf'),
-    })
+    });
 
     return (
-        <Provider profileStore={profileStore}>
-            <MainNavigation />
-        </Provider>
-    )
+        <QueryClientProvider client={queryClient}>
+            <Provider profileStore={profileStore}>
+                <MainNavigation />
+            </Provider>
+        </QueryClientProvider>
+    );
 }
