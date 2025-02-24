eb0fdccdb74cd4acc5e442544d189d39f124ea5d
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-24 22:56:12 +0900

    feat : AlertModal 컴포넌트 추가

---
+++ src/components/common/modals/AlertModal.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    TextStyle,
+    StyleProp,
+    StyleSheet,
+} from 'react-native';
+
+interface AlertModalProps {
+    message: string;
+    leftButtonText: string;
+    rightButtonText: string;
+    onLeftClicked: () => void; // '나가기' 버튼 클릭 시 동작
+    onRightClicked: () => void; // '이어서 쓰기' 버튼 클릭 시 동작
+}
+
+const AlertModal: React.FC<AlertModalProps> = ({
+    message,
+    leftButtonText,
+    rightButtonText,
+    onLeftClicked,
+    onRightClicked,
+}) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.messageContainer}>
+                <Text style={text.messageText}>{message}</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                    style={styles.cancelButton}
+                    onPress={onLeftClicked}
+                >
+                    <Text style={text.cancelText}>{leftButtonText}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.continueButton}
+                    onPress={onRightClicked}
+                >
+                    <Text style={text.continueText}>{rightButtonText}</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+export default AlertModal;
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        width: 364,
+        height: 153,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFF',
+        borderRadius: 12,
+    },
+    messageContainer: {
+        width: 364,
+        height: 97,
+        padding: 16,
+        borderTopLeftRadius: 12,
+        borderTopRightRadius: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: 364,
+        height: 56,
+        borderBottomLeftRadius: 12,
+        borderBottomRightRadius: 12,
+    },
+    cancelButton: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#EEE',
+        borderBottomLeftRadius: 12,
+    },
+    continueButton: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#52A55D',
+        borderBottomRightRadius: 12,
+    },
+});
+
+const text = {
+    fontFamily: 'Pretendard',
+    fontStyle: 'normal',
+    messageText: {
+        color: '#232323',
+        fontSize: 18,
+        fontWeight: '600',
+        lineHeight: 24,
+    },
+    cancelText: {
+        color: '#555',
+        fontSize: 18,
+        fontWeight: '600',
+        lineHeight: 24,
+    },
+    continueText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: '600',
+        lineHeight: 24,
+    },
+} as {
+    [key: string]: StyleProp<TextStyle>;
+};
