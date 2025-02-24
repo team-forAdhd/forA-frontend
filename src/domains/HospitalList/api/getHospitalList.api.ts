e99d2cd0a772b71c428381b21fbbbc297f4220d3
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-24 23:00:58 +0900

    feat :병원 목록 query hook 추가

---
+++ src/domains/HospitalList/api/getHospitalList.api.ts
@@ -0,0 +1,67 @@
+import {
+    InfiniteData,
+    useInfiniteQuery,
+    UseInfiniteQueryOptions,
+    UseInfiniteQueryResult,
+} from '@tanstack/react-query';
+import { apiClient } from '../../../api/login/loginApi';
+import { Hospital } from '@/components/hospital/types';
+
+type GetHospitalListRequest = {
+    latitude: number;
+    longitude: number;
+    radius: number;
+    page: number;
+    size: number;
+    sort: string;
+    filter: string;
+};
+
+type Paging = {
+    size: number;
+    totalPages: number;
+    numberOfElements: number;
+    totalElements: number;
+    isFirst: boolean;
+    isLast: boolean;
+    isEmpty: boolean;
+};
+
+type GetHospitalListResponse = {
+    hospitalList: Hospital[];
+    paging: Paging;
+};
+
+// 병원 목록을 가져오는 함수
+const SORT_OPTION = {
+    REVIEW_DESC: 'reviewCount,desc',
+};
+export const getHospitalList = async ({
+    filter,
+    latitude,
+    longitude,
+    page,
+    radius,
+    size,
+    sort,
+}: GetHospitalListRequest): Promise<GetHospitalListResponse> => {
+    const { data } = await apiClient.get<GetHospitalListResponse>(
+        `/hospitals/nearby?latitude=${latitude}&longitude=${longitude}&radius=${radius}&page=${page}&size=${size}&sort=${SORT_OPTION['REVIEW_DESC']}&filter=${filter}`,
+    );
+    return data;
+};
+
+export const useHospitalList = (
+    params: Omit<GetHospitalListRequest, 'page'>,
+): UseInfiniteQueryResult<InfiniteData<GetHospitalListResponse, number>> => {
+    return useInfiniteQuery({
+        queryKey: ['hospitalList', params.latitude, params.longitude],
+        queryFn: ({ pageParam }) =>
+            getHospitalList({ ...params, page: pageParam as number }),
+        initialPageParam: 1,
+        getNextPageParam: (lastPage, allPages) => {
+            if (lastPage.paging.isLast) return undefined;
+            return allPages.length + 1;
+        },
+    });
+};
