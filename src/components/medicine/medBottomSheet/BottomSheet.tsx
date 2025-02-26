17113a070be0b8da176c51bb1346742aa280c8d1
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-26 17:35:00 +0900

    refactor : BottomSheet 컴포넌트의 스타일 및 로직 개선

--- src/components/medicine/medBottomSheet/BottomSheet.tsx
+++ src/components/medicine/medBottomSheet/BottomSheet.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from 'react';
 import {
     View,
     Text,
     TouchableOpacity,
     Modal,
-    Image,
     FlatList,
-} from 'react-native'
-import { styles, text } from './BottomSheetStyle'
+    Pressable,
+    StyleSheet,
+    TextStyle,
+    StyleProp,
+} from 'react-native';
 
 interface BottomSheetProps {
-    visible: boolean
-    onClose: () => void
-    options: string[]
-    onSelect: (option: string) => void
-    selectedOption: string
+    visible: boolean;
+    onClose: () => void;
+    options: string[];
+    onSelect: (option: string) => void;
+    selectedOption: string;
 }
 
 export default function BottomSheet({
@@ -31,15 +33,11 @@ export default function BottomSheet({
             visible={visible}
             onRequestClose={onClose}
         >
-            <View style={styles.overlay}>
+            <Pressable style={styles.overlay} onPress={onClose}>
                 <View style={styles.container}>
                     <View style={styles.titleContainer}>
-                        <Image
-                            style={styles.topLine}
-                            source={require('@/public/assets/rangeTop.png')}
-                        />
                         {selectedOption != '차단하기' && (
-                            <Text style={text.titleText}>정렬 옵션</Text>
+                            <Text style={text.titleText}>게시글 관리</Text>
                         )}
                     </View>
                     <View style={styles.flatList}>
@@ -50,31 +48,115 @@ export default function BottomSheet({
                                 <TouchableOpacity
                                     style={styles.optionContainer}
                                     onPress={() => {
-                                        onSelect(item)
-                                        onClose() // 선택 후 바텀시트 닫기
+                                        onSelect(item);
+                                        onClose(); // 선택 후 바텀시트 닫기
                                     }}
                                 >
-                                    <Text
-                                        style={
-                                            item === selectedOption
-                                                ? text.checkText
-                                                : text.commonText
-                                        }
-                                    >
-                                        {item}
-                                    </Text>
-                                    {item === selectedOption && (
-                                        <Image
-                                            source={require('@/public/assets/check.png')}
-                                            style={styles.checkIcon}
-                                        />
-                                    )}
+                                    <Text style={text.commonText}>{item}</Text>
                                 </TouchableOpacity>
                             )}
                         />
                     </View>
                 </View>
-            </View>
+            </Pressable>
         </Modal>
-    )
+    );
 }
+
+// MedSelectModalStyle.ts
+
+const styles = StyleSheet.create({
+    overlay: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    container: {
+        backgroundColor: 'white',
+        padding: 20,
+        borderWidth: 1,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+    },
+    optionContainer: {
+        left: 8,
+        paddingVertical: 15,
+        flexDirection: 'row',
+        // borderBottomWidth: 1,
+        // borderBottomColor: '#eee',
+    },
+    titleContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        paddingHorizontal: 16,
+        width: '100%',
+        height: 25,
+    },
+    topLine: {
+        marginTop: 12,
+        width: 80,
+        height: 4,
+        borderRadius: 500,
+        left: 10,
+        marginBottom: 12,
+    },
+    rangeInnerContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 32,
+        paddingVertical: 22,
+    },
+    checkIcon: {
+        width: 24,
+        height: 24,
+        left: 250,
+    },
+    flatList: {
+        marginTop: 10,
+    },
+});
+
+const baseText = {
+    fontFamily: 'Pretendard',
+    fontStyle: 'normal',
+    fontWeight: '400',
+};
+
+const text = {
+    titleText: {
+        fontWeight: '600',
+        color: '#232323',
+        fontSize: 18,
+        lineHeight: 22.4,
+        right: -8,
+    },
+    commonText: {
+        ...baseText,
+        fontSize: 18,
+        lineHeight: 22.4,
+        letterSpacing: -0.9,
+        color: '#232323',
+    },
+    checkText: {
+        fontWeight: '600',
+        color: '#52A55D',
+        fontSize: 18,
+        lineHeight: 24,
+    },
+    viewContentText: {
+        ...baseText,
+        color: '#52A55D',
+        fontSize: 14,
+        lineHeight: 19.6,
+        letterSpacing: -0.7,
+        textDecorationLine: 'underline',
+    },
+    businessInfoText: {
+        ...baseText,
+        color: '#949494',
+        textAlign: 'center',
+    },
+} as {
+    [key: string]: StyleProp<TextStyle>;
+};
