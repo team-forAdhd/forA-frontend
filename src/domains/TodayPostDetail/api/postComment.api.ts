654d69ece4eec1f62b227ec68a0db287af2c913f
Minhyung Cho <dup33333@hufs.ac.kr>
2025-02-26 17:36:12 +0900

    feat : 댓글 등록 API 및 훅 추가

---
+++ src/domains/TodayPostDetail/api/postComment.api.ts
@@ -0,0 +1,34 @@
+import { apiClient } from '@/api/login/loginApi';
+import { API_URL } from '@env';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { Alert } from 'react-native';
+
+type PostCommentRequest = {
+    postId: number;
+    content: string;
+    isAnonymous: boolean;
+    parentCommentId?: number;
+};
+
+export function sendCommentApi(commentData: PostCommentRequest) {
+    return apiClient.post(`${API_URL}/api/v1/comments`, commentData);
+}
+
+export function useCommentMutation(postId: number) {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: sendCommentApi,
+        onSuccess: async () => {
+            Alert.alert('댓글이 등록되었습니다.');
+            await queryClient.invalidateQueries({
+                queryKey: ['todayPostDetail', postId],
+            });
+        },
+        onError: () => {
+            Alert.alert(
+                '댓글 등록 중 오류가 발생했습니다.',
+                '다시 시도해주세요.',
+            );
+        },
+    });
+}
