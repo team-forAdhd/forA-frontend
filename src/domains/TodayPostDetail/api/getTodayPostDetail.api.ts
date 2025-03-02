20aa6f67c14fec0847317e29e26c3b7508ddaa92
Minhyung Cho <dup33333@hufs.ac.kr>
2025-03-02 16:32:53 +0900

    feat: getTodayPostDetail API에 포스트 ID가 -1일 때 알림 반환 추가 및 useTodayPostDetail 훅 수정

--- src/domains/TodayPostDetail/api/getTodayPostDetail.api.ts
+++ src/domains/TodayPostDetail/api/getTodayPostDetail.api.ts
@@ -2,6 +2,10 @@ import { API_URL } from '@env';
 import { apiClient } from '@/api/login/loginApi';
 import { Post } from '@/domains/TodayPostDetail/types/todayPostDetail.types';
 import { useQuery } from '@tanstack/react-query';
+import { notification } from '@/domains/TodayPostDetail/utils/notifications';
+import { useNavigation } from '@react-navigation/native';
+import { StackScreenProps } from '@react-navigation/stack';
+import { TodayStackParams } from '@/navigation/stacks/TodayStack';
 
 const categoryMap: { [key: string]: string } = {
     TEENS: '10대',
@@ -14,11 +18,14 @@ const convertCategory = (category: string): string => {
 };
 
 export async function getTodayPostDetail(postId: number): Promise<Post> {
+    if (postId === -1) return notification;
     const { data } = await apiClient.get(`${API_URL}/api/v1/posts/${postId}`);
-    return data;
+    return { ...data, category: categoryMap[data.category] };
 }
 
 export function useTodayPostDetail(postId: number) {
+    const navigation =
+        useNavigation<StackScreenProps<TodayStackParams, 'PostDetail'>>();
     return useQuery({
         queryKey: ['todayPostDetail', postId],
         queryFn: () => getTodayPostDetail(postId),
