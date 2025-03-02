ec5b66e40f7a8dae090dee62b07ecfeaa6b74e05
Minhyung Cho <dup33333@hufs.ac.kr>
2025-03-02 16:27:46 +0900

    refactor: 코드 스타일 개선 및 불필요한 주석 제거

--- src/components/common/modals/loadingModal.tsx
+++ src/components/common/modals/loadingModal.tsx
@@ -16,6 +16,7 @@ const styles = StyleSheet.create({
         height: '100%',
         justifyContent: 'center',
         alignItems: 'center',
+        zIndex: 50,
     },
     horizontal: {
         flexDirection: 'row',
