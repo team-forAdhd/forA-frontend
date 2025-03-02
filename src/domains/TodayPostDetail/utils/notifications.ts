affc98252b2bf1782026fffbabc7d77717774325
Minhyung Cho <dup33333@hufs.ac.kr>
2025-03-02 16:30:40 +0900

    feat: 오늘탭 글 중 공지글 변경

---
+++ src/domains/TodayPostDetail/utils/notifications.ts
@@ -0,0 +1,28 @@
+export const notification = {
+    id: -1,
+    userId: '운영자',
+    title: '오늘탭 작성가이드 (필독!)',
+    content: `안녕하세요, 포에이 담당자입니다.
+‘포에이’ 이용에 불편이 없기를 바라며, 오늘탭 작성가이드를 알려드립니다\n
+1. 카테고리에 맞게 글을 작성해주세요. ADHD의 증상은 나이대별로 상이하기에 10대, 20대, 30대 이상, 학부모용으로 나뉩니다.\n
+2. 민감한 글, 욕설, 혐짤, 수위가 있는 내용은 반드시 5칸 이상 내려쓰세요. (밑에 예시 참고)
+제목은 [!]으로 표시해주세요. 어길 시 게시글은 삭제됩니다.
+.
+.
+.
+.
+.\n
+3. 자살, 살인, 담당자 판단하에 수위가 높은 글 등의 내용을 담은 게시글은 무조건 삭제됩니다. 예를 들어, 힘들다, 우울하다 정도는 괜찮으나 죽고 싶어, 죽이고 싶어 혹은 자살방법을 묻는 글은 금지됩니다.\n`,
+    anonymous: true,
+    images: null,
+    likeCount: 0,
+    commentCount: 2,
+    scrapCount: 0,
+    viewCount: 1,
+    category: '공지',
+    comments: [],
+    nickname: null,
+    profileImage: null,
+    createdAt: 1723046942,
+    lastModifiedAt: 1723046949,
+};
