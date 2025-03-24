097e190c41d7407dea2b9f53abab522250db2df3
Minhyung Cho <dup33333@hufs.ac.kr>
2025-03-24 11:06:34 +0900

    feat: 페이징 타입 정의 추가

---
+++ src/types/paging.ts
@@ -0,0 +1,10 @@
+export type Paging = {
+    page: number;
+    size: number;
+    totalPages: number;
+    numberOfElements: number;
+    totalElements: number;
+    isFirst: boolean;
+    isLast: boolean;
+    isEmpty: boolean;
+};
