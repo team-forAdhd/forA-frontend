62f8c8f12ab7fcabb05d50e0b19081f9ba6d4c0f
JInsunnya <jsk5754@naver.com>
2025-05-09 21:06:53 +0900

    feat: 홈 배너 인스타그램 외부 링크 및 약탭 내부 이동 기능 추가

--- src/components/common/carousel/carousel.tsx
+++ src/components/common/carousel/carousel.tsx
@@ -4,6 +4,7 @@ import Carousel from 'react-native-snap-carousel';
 import { styles } from './carouselStyle';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
+import { Linking } from 'react-native';
 
 interface CarouselItem {
     source: { uri: string };
@@ -20,7 +21,8 @@ export default function CarouselComponent({ setModalTitle }: CarouselProps) {
         { source: require('@/public/assets/carousel1.png'), name: '인스타' },
         { source: require('@/public/assets/carousel2.png'), name: '약탭' },
         { source: require('@/public/assets/carousel3.png'), name: '공지사항' },
-        //{ source: require('@/public/assets/carousel4.png'), name: '후원' },
+        // { source: require('@/public/assets/carousel4.png'), name: '후원' },
+        { source: require('@/public/assets/carousel5.png'), name: '이벤트' },
     ];
 
     const renderItem = ({ item }: { item: CarouselItem }) => {
@@ -29,16 +31,29 @@ export default function CarouselComponent({ setModalTitle }: CarouselProps) {
                 <TouchableOpacity
                     onPress={() => {
                         if (item.name === '약탭') {
-                            navigation.navigate('MedicineMain' as never);
+                            navigation.navigate('약' as never);
                         } else if (item.name === '공지사항') {
                             setModalTitle('notification');
-                        } else if (item.name === '후원') {
-                            //setModalTitle('support')
+                            // } else if (item.name === '후원') {
+                            //     //setModalTitle('support')
+                            // }
+                        } else if (item.name === '인스타') {
+                            Linking.openURL(
+                                'https://www.instagram.com/for_adhd?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==',
+                            );
+                        } else if (item.name === '이벤트') {
+                            Linking.openURL(
+                                'https://www.instagram.com/p/DJbPaGxSw11/?igsh=MWg0ejZ0ODNwMmhveA==',
+                            );
                         }
                     }}
                     style={{ aspectRatio: 16 / 9, width: '100%' }}
                 >
-                    <Image source={item.source} style={styles.carouselImage} />
+                    <Image
+                        source={item.source}
+                        style={styles.carouselImage}
+                        resizeMode="contain"
+                    />
                 </TouchableOpacity>
             </View>
         );
